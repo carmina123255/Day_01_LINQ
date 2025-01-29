e8d3c89239ec33603987c7237efa9fa04ea2734e
carmi <carmi@CERMINA_MAGED>
2025-01-29 18:25:26 +0200

    Anonymous Type

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -35,6 +35,28 @@
             #endregion
 
 
+            #region Anonymous Type 
+            ///    object emp01 = new { Id = 10, Name = "aa", Salary = 80_000 };
+            ///    //Console.WriteLine(emp01.Id) //InValid
+            /// 
+            ///    var Employee = new { Id = 10, Name = "aa", Salary = 80_000 };
+            ///    Console.WriteLine(Employee.Id);
+            ///    Console.WriteLine(Employee.GetType().Name);//AnonymousType0`3
+            ///    var Employee02 = new { Id = 10, Name = "bb", Salary = 80_000F };
+            ///    Console.WriteLine(Employee02.GetType().Name);//AnonymousType0`3
+            ///    //The Same Anonymous Type As long AS :
+            ///    //1-the same Properties Naming[case Sensitive]
+            ///    //2-the same properties
+            ///    var Employee03 = new { Id = 10, Salary = 80_000, Name = "kermina" };
+            /// 
+            ///    Console.WriteLine(Employee03.GetType().Name);//AnonymousType1`3
+            ///    var Product = new { ProductId = 100, ProductName = "TEA" };
+            ///    Console.WriteLine(Product.GetType().Name);//AnonymousType2`2 
+            #endregion
+
+
+
+
 
         }
     }
