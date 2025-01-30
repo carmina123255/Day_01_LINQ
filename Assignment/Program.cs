953e3de458d07626b5f7c77812f2f7e0bc5b1faa
carmi <carmi@CERMINA_MAGED>
2025-01-30 11:03:36 +0200

    Q01 Part01

---
+++ Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using static Assignment.ListGenerator;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01 
+
+            #region Q1 
+            ///Find all products that are out of stock.
+            ///
+
+            ///   var Result = ProductList.Where(P => P.UnitsInStock == 0);
+            ///
+            ///   Result = from p in ProductList
+            ///            where p.UnitsInStock == 0
+            ///            select p;
+            ///
+            ///   foreach (var item in Result) Console.WriteLine(item); 
+            #endregion
+
+
+            #endregion
+        }
+    }
+}
